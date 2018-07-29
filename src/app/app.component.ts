c2051244a9e1d36f55bbd733a198600fb8553b67
Yoav Gurevich <ygurevich@ymail.com>
2018-07-29 12:16:24 -0400

    Implemented dynamic tooltip placement; Style update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,19 @@ export class AppComponent implements OnInit {
   version = 6;
   buttons = [
     {
+      id: this.generateId(),
       name: 'Work Pro Tip',
       placement: 'bottom',
       tooltip: 'Have the wisdom of a 50 year old, the experience of a 40 year old, and the focus of a 30 year old!'
     },
     {
+      id: this.generateId(),
       name: 'Life Pro Tip',
-      placement: 'top',
       tooltip: 'Moderation, friends, and lots of patience!'
     },
     {
+      id: this.generateId(),
       name: 'Love Pro Tip',
-      placement: 'right',
       tooltip: 'Courage, diplomacy, and lots of gifts!'
     }
   ];
@@ -36,5 +37,36 @@ export class AppComponent implements OnInit {
       const activeElm = (document.activeElement as any);
       activeElm.blur();
     }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll(event) {
+    const buttonElms = Array.from(document.body.getElementsByTagName('button'));
+
+    if (buttonElms.length) {
+      buttonElms.forEach(element => {
+        const elmOffsets = element.getBoundingClientRect();
+
+        if (elmOffsets.top < 150) {
+          const foundBtn = this.buttons.find(button => button.id === element.id);
+
+          if (foundBtn && foundBtn.placement === 'top') {
+            foundBtn.placement = 'bottom';
+          }
+        } else {
+          const foundBtn = this.buttons.find(button => button.id === element.id);
+
+          if (foundBtn && foundBtn.placement === 'bottom') {
+            foundBtn.placement = 'top';
+          }
+        }
+      });
+    }
+  }
+
+  generateId() {
+    const arr = new Uint8Array(8);
+    window.crypto.getRandomValues(arr);
+    return arr.join('');
   }
 }
